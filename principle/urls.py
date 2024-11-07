7ef978af6baa931f36d28578090af49e5a54cc67
JawwadKhan060790 <81690099+JawwadKhan060790@users.noreply.github.com>
2024-11-07 18:51:08 +0530

    new login and html changes

--- principle/urls.py
+++ principle/urls.py
@@ -4,7 +4,8 @@ from principle import views
 urlpatterns = [
 
     path('create-teacher', views.create_teacher,name='create-teacher'),
+    path('test-list', views.test_list,name='test-list'),
     path('teacher-list', views.teacher_list,name='teacher-list'),
     path('teacher-list-pending', views.teacher_list_pending,name='teacher-list-pending'),
-    
+   
 ]
